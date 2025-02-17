952e158283935e7a15325acfd22c9e89babdb555
Prom88 <endersteve607@gmail.com>
2025-02-18 00:41:44 +0300

    fastfix

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -25,7 +25,7 @@ function SearchBar() {
 				/>
 			</div>
 			<div className={styles.buttonBox}>
-				<Button variant='contained' onChange={() => dispatch(setQuery(name))}>
+				<Button variant='contained' onClick={() => dispatch(setQuery(name))}>
 					ИСКАТЬ
 				</Button>
 			</div>
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/RepoSearcher',
+	base: '/RepoSearcher-byUsername-',
 	build: {
 		outDir: 'build', // имя общей выходной директории
 	},
